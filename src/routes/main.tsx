2fafa9f2767e753b50bdb89126f4b0cdba4df7a9
neistow <glebchernigin236@gmail.com>
2023-02-23 12:11:11 +0200

    One does not simply merge into master

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -2,18 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Item } from '../types';
 import CategoryList from '../components/CategoryList';
 import ItemList from '../components/ItemList';
+import { queryItems } from '../api/mocks';
 
 export default function Main() {
     const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(null);
     const [items, setItems] = useState<Item[]>([]);
 
     useEffect(() => {
-        const url = new URL('http://localhost:4000/items');
-        if (selectedCategoryId !== null) {
-            url.searchParams.set('categoryId', selectedCategoryId.toString())
-        }
-        fetch(url.toString())
-            .then(r => r.json())
+        queryItems(selectedCategoryId)
             .then(d => setItems(d));
     }, [selectedCategoryId]);
 
