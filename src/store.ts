85a6761db568fc5b1c87c0e43d20b4a7a942471e
neistow <glebchernigin236@gmail.com>
2023-04-07 13:26:29 +0300

    fix

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer, { Theme } from './themeSlice';
-<<<<<<< Updated upstream
-
-export type RootState = {
-    theme: Theme
-=======
 import { productsApi } from './services/products';
 import { toDoSaga, toDoSlice, ToDoState } from './todosSlice';
 import createSagaMiddleware from 'redux-saga';
@@ -14,7 +9,6 @@ const sagaMiddleware = createSagaMiddleware();
 export type RootState = {
     theme: Theme,
     todos: ToDoState
->>>>>>> Stashed changes
 }
 
 const initialState = {
@@ -24,11 +18,8 @@ const initialState = {
 const store = configureStore({
     reducer: {
         theme: themeReducer,
-<<<<<<< Updated upstream
-=======
         todos: toDoSlice.reducer,
         [productsApi.reducerPath]: productsApi.reducer
->>>>>>> Stashed changes
     },
     preloadedState: initialState as any,
     middleware: (getDefaultMiddleware) =>
